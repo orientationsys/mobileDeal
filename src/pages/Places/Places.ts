796572b17835f8f22120b1e86eb6ef29deecfa08
Adam <wengjincheng@wengjinchengdeMacBook-Pro.local>
2017-03-16 11:05:56 +0800

    map

--- src/pages/Places/Places.ts
+++ src/pages/Places/Places.ts
@@ -12,11 +12,30 @@ import { MapPage } from '../map/map'
   styleUrls: ['/pages/Places/Places.scss']
 })
 export class PlacesPage implements OnInit{
+  //头部导航条图片链接以及isActive的true or false
+  isACT1: any = true;
+  isACT2: any = false;
+  isACT3: any = false;
+  isACT4: any = false;
+  nvImg1: any = 'assets/img/icon-nav-new-1.png';
+  nvImg2: any = 'assets/img/icon-nav-2.png';
+  nvImg3: any = 'assets/img/icon-nav-3.png';
+  nvImg4: any = 'assets/img/icon-nav-4.png';
+  nvImgAct1: any = 'assets/img/icon-nav-new-1.png';
+  nvImgAct2: any = 'assets/img/icon-nav-new-2.png';
+  nvImgAct3: any = 'assets/img/icon-nav-new-3.png';
+  nvImgAct4: any = 'assets/img/icon-nav-new-4.png';
+
+  //ajax
+
   data: any;
   companies:any;
   open:any;
   distances:any;
   BASE_URL:any;
+  deals:any;
+  media:any;
+  mealTime:any;
   constructor(private service: Service,public navCtrl: NavController) {
   }
   PlacesUrl:any = 'http://mobiledeals.sooperior.com/searchRestaurant?city=Windsor&start=0&address=3160 wildwood&state=Ontario';
@@ -30,22 +49,74 @@ export class PlacesPage implements OnInit{
           this.BASE_URL = data.BASE_URL;
         });
   }
-  // categoryFilter(event, category){
-  //   this.service.getCategoryDeals(category)
-  //     .subscribe(
-  //       deals => {
-  //         this.deals = deals;
-  //         this.deals1 = this.deals[0];
-  //         this.baseurl = this.deals[4];
-  //         this.open = this.deals[2];
-  //         this.distances = this.deals[1];
-  //         this.media = this.deals[5];
-  //         this.mealTime = this.deals[3];
-  //       });
-  // }
+  //go to map
   goMap(){
     this.navCtrl.push(MapPage);
 
+  }
+  //切换列表所发送的ajax请求
+  categoryFilter(event, category,act){
+    this.isACT1 = false;
+    this.isACT2 = false;
+    this.isACT3 = false;
+    this.isACT4 = false;
+    if(act=='act1'){
+      this.nvImg2 = 'assets/img/icon-nav-2.png';
+      this.nvImg3 = 'assets/img/icon-nav-3.png';
+      this.nvImg4 = 'assets/img/icon-nav-4.png';
+      this.isACT1 = !this.isACT1;
+      if(this.nvImg1 == 'assets/img/icon-nav-1.png'){
+        this.nvImg1 = this.nvImgAct1;
+      }else if(this.isACT1 == 'false'){
+        this.nvImg1 = 'assets/img/icon-nav-1.png';
+      }
+    }else if(act=='act2'){
+      this.nvImg1 = 'assets/img/icon-nav-1.png';
+      this.nvImg3 = 'assets/img/icon-nav-3.png';
+      this.nvImg4 = 'assets/img/icon-nav-4.png';
+      this.isACT2 = !this.isACT2;
+      if(this.nvImg2 == 'assets/img/icon-nav-2.png'){
+        this.nvImg2 = this.nvImgAct2;
+      }else if(this.isACT2 == 'false'){
+        this.nvImg2 = 'assets/img/icon-nav-2.png';
+      }
+    }else if(act=='act3'){
+      this.nvImg1 = 'assets/img/icon-nav-1.png';
+      this.nvImg2 = 'assets/img/icon-nav-2.png';
+      this.nvImg4 = 'assets/img/icon-nav-4.png';
+      this.isACT3 = !this.isACT3;
+      if(this.nvImg3 == 'assets/img/icon-nav-3.png'){
+        this.nvImg3 = this.nvImgAct3;
+      }else if(this.isACT3 == 'false'){
+        this.nvImg3 = 'assets/img/icon-nav-3.png';
+      }
+    }
+    this.service.getCategoryDeals(category)
+          .subscribe(
+              data => {
+                this.data = data;
+                this.deals = data.deals;
+                this.BASE_URL = data.BASE_URL;
+                this.open = data.open;
+                this.distances = data.distances;
+                this.media = data.media;
+                this.mealTime = data.mealTime;
+                  });
+  }
+  actFilter(){
+    this.isACT1 = false;
+    this.isACT2 = false;
+    this.isACT3 = false;
+    this.isACT4 = false;
+    this.nvImg1 = 'assets/img/icon-nav-1.png';
+    this.nvImg2 = 'assets/img/icon-nav-2.png';
+    this.nvImg3 = 'assets/img/icon-nav-3.png';
+    this.isACT4 = !this.isACT4;
+    if(this.nvImg4 == 'assets/img/icon-nav-4.png'){
+      this.nvImg4 = this.nvImgAct4;
+    }else if(this.isACT4 == 'false'){
+      this.nvImg4 = 'assets/img/icon-nav-4.png';
+    }
   }
   ngOnInit(): void {
     this.getPlaces();
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -22,11 +22,27 @@ export class MapPage implements OnInit{
     let map = Leaflet.map('map');
     Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      center: [-122.420679, 37.772537],
+      maxZoom: 18
     }).addTo(map);
 
     //web location
     map.locate({ setView: true});
+
+    //when we have a location draw a marker and accuracy circle
+    function onLocationFound(e) {
+      var radius = e.accuracy / 2;
+
+      Leaflet.marker(e.latlng).addTo(map)
+          .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+      Leaflet.circle(e.latlng, radius).addTo(map);
+    }
+    map.on('locationfound', onLocationFound);
+    //alert on location error
+    function onLocationError(e) {
+      alert(e.message);
+    }
+
+    map.on('locationerror', onLocationError);
   }
 }
