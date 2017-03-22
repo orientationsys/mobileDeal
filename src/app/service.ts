3cd5dde336717f20022752b35a1191a633855a0b
xiaofei <1355303252@qq.com>
2017-03-22 14:16:39 +0800

    css

--- src/app/service.ts
+++ src/app/service.ts
@@ -15,30 +15,62 @@ export class Service {
   constructor(private http: Http) { }
 
   data:Deals;
+  //拿到deals数据
   getDeals(): Observable<Deals> {
     return this.http.get(this.getCityUrl)
-      .map(this.extractData).catch(this.handleError);
+        .map(this.extractData).catch(this.handleError);
   }
+  private extractData(res: Response) {
+    let body = res.json();
+    let list: any = {deals:body.deals,distances:body.distances,open:body.open,mealTime:body.mealTime,BASE_URL:body.BASE_URL,media:body.media};
+    return list;
+  }
+  //拿取detailPromos页面数据
   getDetailPromos(url): Observable<Deals>{
     return this.http.get(url)
-    .map(this.extractDetailPromosData).catch(this.handleError);
+        .map(this.extractDetailPromosData).catch(this.handleError);
   }
+  private extractDetailPromosData(res: Response){
+    let body = res.json();
+    let list: any = {medias:body.medias,moreDeals:body.moreDeals,BASE_URL:body.BASE_URL};
+    return list;
+  }
+
+  //获取promos页面导航菜单的数据
   getCategoryDeals(category): Observable<Deals> {
     return this.http.get(this.getCategoryUrl+category)
         .map(this.extractData).catch(this.handleError);
   }
+  //获取Blog页面的数据
   getBlogs(url):Observable<Deals> {
     return this.http.get(url)
         .map(this.extractBlogData).catch(this.handleError);
   }
+  private extractBlogData(res: Response) {
+    let body = res.json();
+    return body.foods;
+  }
+  //获取Blog的detail数据
   getDetailBlog(url):Observable<Deals> {
     return this.http.get(url)
         .map(this.extractBlogDetail).catch(this.handleError);
   }
+  private extractBlogDetail(res: Response) {
+    let body = res.json();
+    let list:any  =  {food:body.food,BASE_URL:body.BASE_URL};
+    return list;
+  }
+  //获取Resturants页面数据
   getResturants(url){
     return this.http.get(url)
         .map(this.extractResturants).catch(this.handleError);
   }
+  private extractResturants(res: Response){
+    let body = res.json();
+    let list: any  = {company:body.company,BASE_URL:body.BASE_URL,open:body.open,deals:body.deals};
+    return list;
+  }
+  //获取resturnats-media 的 Gallery
   getResturantGallery(url){
     return this.http.get(url)
         .map(this.extractResturantGallery).catch(this.handleError);
@@ -48,52 +80,32 @@ export class Service {
     let list: any  = {company:body.company,BASE_URL:body.BASE_URL,medias:body.medias};
     return list;
   }
-  private extractBlogDetail(res: Response) {
-    let body = res.json();
-    let list:any  =  {food:body.food,BASE_URL:body.BASE_URL};
-    return list;
-  }
-  private extractBlogData(res: Response) {
-    let body = res.json();
-    return body.foods;
-  }
+  //获取places页面的数据
   getPlaces(PlacesUrl): Observable<Deals> {
     return this.http.get(PlacesUrl)
-      .map(this.placesData).catch(this.handleError);
-  }
-  getCategoryPlaces(url): Observable<Deals> {
-    return this.http.get(url)
         .map(this.placesData).catch(this.handleError);
-  }
-  getResturantsMenu(url): Observable<Deals> {
-    return this.http.get(url)
-      .map(this.extractResturantsMenu).catch(this.handleError);
   }
   private placesData(res: Response){
     let body = res.json();
     let list:any  =  {companies:body.companies,distances:body.distances,open:body.open,BASE_URL:body.BASE_URL};
     return list;
   }
-  private extractData(res: Response) {
-    let body = res.json();
-    let list: any = {deals:body.deals,distances:body.distances,open:body.open,mealTime:body.mealTime,BASE_URL:body.BASE_URL,media:body.media};
-    return list;
+  //获取places页面导航菜单数据
+  getCategoryPlaces(url): Observable<Deals> {
+    return this.http.get(url)
+        .map(this.placesData).catch(this.handleError);
   }
-  private extractDetailPromosData(res: Response){
-    let body = res.json();
-    let list: any = {medias:body.medias,moreDeals:body.moreDeals,BASE_URL:body.BASE_URL};
-    return list;
-  }
-  private extractResturants(res: Response){
-    let body = res.json();
-    let list: any  = {company:body.company,BASE_URL:body.BASE_URL,open:body.open,deals:body.deals};
-    return list;
+  //获取Resturants页面menu的数据
+  getResturantsMenu(url): Observable<Deals> {
+    return this.http.get(url)
+        .map(this.extractResturantsMenu).catch(this.handleError);
   }
   private extractResturantsMenu(res:Response){
     let body = res.json();
     let list:any = {menus:body.menus,BASE_URL:body.BASE_URL,deals:body.deals};
     return list;
   }
+  //处理错误信息的Function
   private handleError (error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
     let errMsg: string;
--- src/pages/Promos/Promos.ts
+++ src/pages/Promos/Promos.ts
@@ -9,19 +9,7 @@ import { PromosPreviewPage } from '../../pages/promos-preview/promos-preview';
   templateUrl: 'Promos.html'
 })
 export class PromosPage implements OnInit{
-  //头部导航条图片链接以及isActive的true or false
-  isACT1: any = false;
-  isACT2: any = false;
-  isACT3: any = false;
-  isACT4: any = false;
-  nvImg1: any = 'assets/img/icon-nav-1.png';
-  nvImg2: any = 'assets/img/icon-nav-2.png';
-  nvImg3: any = 'assets/img/icon-nav-3.png';
-  nvImg4: any = 'assets/img/icon-nav-4.png';
-  nvImgAct1: any = 'assets/img/icon-nav-new-1.png';
-  nvImgAct2: any = 'assets/img/icon-nav-new-2.png';
-  nvImgAct3: any = 'assets/img/icon-nav-new-3.png';
-  nvImgAct4: any = 'assets/img/icon-nav-new-4.png';
+
   //ajax所拿到的各项值
   data:any;
   deals: any;
@@ -31,6 +19,7 @@ export class PromosPage implements OnInit{
   distances:any;
   media:any = [];
   BASE_URL:any;
+  selectCategory:any;
   constructor(private service: Service,public navCtrl: NavController,public modalCtrl: ModalController) {
   }
   //ajax获取deals
@@ -48,42 +37,8 @@ export class PromosPage implements OnInit{
           });
   }
   //切换列表所发送的ajax请求
-  categoryFilter(event, category,act){
-    this.isACT1 = false;
-    this.isACT2 = false;
-    this.isACT3 = false;
-    this.isACT4 = false;
-    if(act=='act1'){
-      this.nvImg2 = 'assets/img/icon-nav-2.png';
-      this.nvImg3 = 'assets/img/icon-nav-3.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT1 = !this.isACT1;
-      if(this.nvImg1 == 'assets/img/icon-nav-1.png'){
-        this.nvImg1 = this.nvImgAct1;
-      }else if(this.isACT1 == 'false'){
-        this.nvImg1 = 'assets/img/icon-nav-1.png';
-      }
-    }else if(act=='act2'){
-      this.nvImg1 = 'assets/img/icon-nav-1.png';
-      this.nvImg3 = 'assets/img/icon-nav-3.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT2 = !this.isACT2;
-      if(this.nvImg2 == 'assets/img/icon-nav-2.png'){
-        this.nvImg2 = this.nvImgAct2;
-      }else if(this.isACT2 == 'false'){
-        this.nvImg2 = 'assets/img/icon-nav-2.png';
-      }
-    }else if(act=='act3'){
-      this.nvImg1 = 'assets/img/icon-nav-1.png';
-      this.nvImg2 = 'assets/img/icon-nav-2.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT3 = !this.isACT3;
-      if(this.nvImg3 == 'assets/img/icon-nav-3.png'){
-        this.nvImg3 = this.nvImgAct3;
-      }else if(this.isACT3 == 'false'){
-        this.nvImg3 = 'assets/img/icon-nav-3.png';
-      }
-    }
+  categoryFilter(event, category){
+    this.selectCategory = category;
     this.service.getCategoryDeals(category)
           .subscribe(
               data => {
@@ -95,21 +50,6 @@ export class PromosPage implements OnInit{
                 this.media = data.media;
                 this.mealTime = data.mealTime;
                   });
-  }
-  actFilter(){
-    this.isACT1 = false;
-    this.isACT2 = false;
-    this.isACT3 = false;
-    this.isACT4 = false;
-    this.nvImg1 = 'assets/img/icon-nav-1.png';
-    this.nvImg2 = 'assets/img/icon-nav-2.png';
-    this.nvImg3 = 'assets/img/icon-nav-3.png';
-    this.isACT4 = !this.isACT4;
-    if(this.nvImg4 == 'assets/img/icon-nav-4.png'){
-      this.nvImg4 = this.nvImgAct4;
-    }else if(this.isACT4 == 'false'){
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-    }
   }
   ngOnInit(): void {
     this.getDeals();
