89ae38981f8c9b14bd23df0b6f9fb416d3075f61
xiaofei <1355303252@qq.com>
2017-03-27 14:15:30 +0800

    filter

--- src/pages/Places/Places.ts
+++ src/pages/Places/Places.ts
@@ -15,18 +15,7 @@ declare var window;
 })
 export class PlacesPage implements OnInit{
   //头部导航条图片链接以及isActive的true or false
-  isACT1: any = false;
-  isACT2: any = false;
-  isACT3: any = false;
-  isACT4: any = false;
-  nvImg1: any = 'assets/img/icon-nav-1.png';
-  nvImg2: any = 'assets/img/icon-nav-2.png';
-  nvImg3: any = 'assets/img/icon-nav-3.png';
-  nvImg4: any = 'assets/img/icon-nav-4.png';
-  nvImgAct1: any = 'assets/img/icon-nav-new-1.png';
-  nvImgAct2: any = 'assets/img/icon-nav-new-2.png';
-  nvImgAct3: any = 'assets/img/icon-nav-new-3.png';
-  nvImgAct4: any = 'assets/img/icon-nav-new-4.png';
+  selectCategory:any;
 
   //ajax
 
@@ -61,45 +50,12 @@ export class PlacesPage implements OnInit{
       this.navCtrl.push(RestaurantsPage,{id:id});
   }
   //切换列表所发送的ajax请求
-  categoryFilter(event, category,act){
-    this.isACT1 = false;
-    this.isACT2 = false;
-    this.isACT3 = false;
-    this.isACT4 = false;
-    if(act=='act1'){
-      this.nvImg2 = 'assets/img/icon-nav-2.png';
-      this.nvImg3 = 'assets/img/icon-nav-3.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT1 = !this.isACT1;
-      if(this.nvImg1 == 'assets/img/icon-nav-1.png'){
-        this.nvImg1 = this.nvImgAct1;
-      }else if(this.isACT1 == 'false'){
-        this.nvImg1 = 'assets/img/icon-nav-1.png';
-      }
-    }else if(act=='act2'){
-      this.nvImg1 = 'assets/img/icon-nav-1.png';
-      this.nvImg3 = 'assets/img/icon-nav-3.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT2 = !this.isACT2;
-      if(this.nvImg2 == 'assets/img/icon-nav-2.png'){
-        this.nvImg2 = this.nvImgAct2;
-      }else if(this.isACT2 == 'false'){
-        this.nvImg2 = 'assets/img/icon-nav-2.png';
-      }
-    }else if(act=='act3'){
-      this.nvImg1 = 'assets/img/icon-nav-1.png';
-      this.nvImg2 = 'assets/img/icon-nav-2.png';
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-      this.isACT3 = !this.isACT3;
-      if(this.nvImg3 == 'assets/img/icon-nav-3.png'){
-        this.nvImg3 = this.nvImgAct3;
-      }else if(this.isACT3 == 'false'){
-        this.nvImg3 = 'assets/img/icon-nav-3.png';
-      }
-    }
+  categoryFilter(event, category){
+    this.selectCategory = category;
     this.service.getCategoryPlaces("http://mobiledeals.sooperior.com/place/getRestaurantsByFilter?city=Windsor&start=0&address=3160 wildwood&state=Ontario&category="+category)
           .subscribe(
               data => {
+                console.log(data);
                 this.companies = data.companies;
                 this.open = data.open;
                 this.distances = data.distances;
@@ -107,19 +63,7 @@ export class PlacesPage implements OnInit{
               });
   }
   actFilter(){
-    this.isACT1 = false;
-    this.isACT2 = false;
-    this.isACT3 = false;
-    this.isACT4 = false;
-    this.nvImg1 = 'assets/img/icon-nav-1.png';
-    this.nvImg2 = 'assets/img/icon-nav-2.png';
-    this.nvImg3 = 'assets/img/icon-nav-3.png';
-    this.isACT4 = !this.isACT4;
-    if(this.nvImg4 == 'assets/img/icon-nav-4.png'){
-      this.nvImg4 = this.nvImgAct4;
-    }else if(this.isACT4 == 'false'){
-      this.nvImg4 = 'assets/img/icon-nav-4.png';
-    }
+
   }
   //生命周期钩子
   ngOnInit(): void {
